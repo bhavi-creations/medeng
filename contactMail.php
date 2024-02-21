b39e624b8f8f7ab3fe6957bc58d1d3626f085562
Rajkumargiduthuri <146897500+Rajkumargiduthuri@users.noreply.github.com>
2024-02-21 11:46:25 +0530

    Update contactMail.php

--- contactMail.php
+++ contactMail.php
@@ -22,14 +22,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'rameshpilli1428@gmail.com'; // Your Gmail email address
-        $mail->Password = 'jjpksiywaevdyyrc'; // Your Gmail password
+        $mail->Username = '@gmail.com'; // Your Gmail email address
+        $mail->Password = ''; // Your Gmail password
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
         // Recipients
-        $mail->setFrom('rameshpilli1428@gmail.com', 'medeng'); // Your Gmail email and name
-        $mail->addAddress('rameshpilli1428@gmail.com', 'medeng'); // Recipient's email and name
+        $mail->setFrom('@gmail.com', 'medeng'); // Your Gmail email and name
+        $mail->addAddress('@gmail.com', 'medeng'); // Recipient's email and name
 
         // Content
         $mail->isHTML(true);
